3c72afa46715bd30df43b4824133e68444f2338d
gekozen <76520530+gekozen@users.noreply.github.com>
2022-09-10 22:00:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var CSS = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("boo");
+
+body.style.background = 
+CSS.textContent = color1.value + " " + color2.value 
+
+function setGradient(){
+	body.style.background = 
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", "
+		+ color2.value 
+		+ ")";
+	CSS.textContent = color1.value + " " + color2.value 
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
