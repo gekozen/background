819ec7e837f6d0228e51d3a4796fa946b0cd01ab
gekozen <76520530+gekozen@users.noreply.github.com>
2022-09-10 21:55:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("boo");
 
+body.style.background = 
+CSS.textContent = color1.value + " " + color2.value 
+
 function setGradient(){
 	body.style.background = 
 		"linear-gradient(to right, " 
@@ -10,10 +13,10 @@ function setGradient(){
 		+ ", "
 		+ color2.value 
 		+ ")";
+	CSS.textContent = color1.value + " " + color2.value 
 }
-/*
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
-*/
